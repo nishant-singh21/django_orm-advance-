e84a3419d1db880d5eb68e1c1e7044a1185137e5
nishant-singh21 <nishantsingh1721@gmail.com>
2026-06-24 12:25:03 +0530

    project18(update operation done)

--- project18/student/urls.py
+++ project18/student/urls.py
@@ -5,4 +5,6 @@ urlpatterns = [
     path('add/', views.student_create, name='student_create'),
     path('', views.student_list, name='student_list'),
     path('detail/<int:pk>/', views.student_detail, name='student_detail'),
+
+    path('edit/<int:pk>/', views.student_edit, name='student_edit'),
     ]
--- project18/student/views.py
+++ project18/student/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 from .forms import StudentForm
 from .models import Student 
 
@@ -22,3 +22,15 @@ def student_list(request):
 def student_detail(request, pk):
     student = get_object_or_404(Student, pk=pk)
     return render(request, 'student_detail.html', {'student': student})
+
+
+def student_edit(request, pk):
+    student = get_object_or_404(Student, pk=pk)
+    if request.method == 'POST':
+        form = StudentForm(request.POST, instance=student)
+        if form.is_valid():
+            form.save()
+            return redirect('student_list')
+    else:
+        form = StudentForm(instance=student)
+    return render(request, 'student_form.html', {'form': form})
